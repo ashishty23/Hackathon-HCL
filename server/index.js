8cdb62b1ef26fbf1a2a5fae319bbc85eff993e88
darsh609 <116168977+darsh609@users.noreply.github.com>
2026-01-17 20:21:40 +0530

    backend setup-part 2

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+
+const cors = require("cors");
+
+app.use(cors({
+  origin: "*", // or your frontend URL
+  credentials: true
+}));
+
+connectDB();
+
+// const { checkApiKey } = require("./middlewares/apiKey.middlewares");
+
+// app.use(checkApiKey);
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running....'
+	});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on porttt ${PORT}`);
+});
+
+module.exports = app;
