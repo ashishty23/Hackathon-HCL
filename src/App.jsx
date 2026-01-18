3b55f0341290214ad4dbad08d13c9928f72d9edc
Vivek Bohra <at760440@gmail.com>
2026-01-18 12:09:12 +0530

    lessgoo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './Navbar'
+import Menu from './Menu'
+import Cart from './Cart'
+import Login from './Login'
+import About from './About'
+import MyProfile from './MyProfile'
+import Home from './Home'
 
 const App = () => {
   return (
-    <div className='text-red-500 font-bold'>App</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/menu' element = {<Menu/>} />
+        <Route path='/Cart' element = {<Cart/>} />
+        <Route path='/login' element = {<Login/>} />
+        <Route path='/about' element = {<About/>} />
+        <Route path='/my-profile' element = {<MyProfile/>} />
+      </Routes>
+      
+    </div>
   )
 }
 
