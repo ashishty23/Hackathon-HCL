75ba5749f27b22be5c2c9300ea90e639aa616431
darsh609 <116168977+darsh609@users.noreply.github.com>
2026-01-18 15:02:51 +0530

    frontend part

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,22 @@ import Login from './Login'
 import About from './About'
 import MyProfile from './MyProfile'
 import Home from './Home'
-
+import PublicRoute from "./routes/PublicRoute";
+import PrivateRoute from "./routes/PrivateRoute";
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Routes>
         <Route path='/' element = {<Home/>} />
-        <Route path='/menu' element = {<Menu/>} />
+       <Route
+  path="/menu"
+  element={
+    <PrivateRoute>
+      <Menu />
+    </PrivateRoute>
+  }
+/>
         <Route path='/Cart' element = {<Cart/>} />
         <Route path='/login' element = {<Login/>} />
         <Route path='/about' element = {<About/>} />
